08b58d4d427d36726a0309e92c01092c2031e540
addis <betsega23@gmail.com>
2022-11-10 23:26:12 +0300

    Implement fizzbuzz method based on the test

--- solver.rb
+++ solver.rb
@@ -1,20 +1,37 @@
 class Solver
-    def factorial(number)
-        if number==0
-            return 1
-        elsif number.negative?
-            return 'No Negative Integers'
-        else
-            number * factorial(number - 1)
-        end
+  def factorial(number)
+    if number.zero?
+      1
+    elsif number.negative?
+      'No Negative Integers'
+    else
+      number * factorial(number - 1)
     end
+  end
 
-    def reverse(str)
-        str.reverse
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number
     end
+  end
 end
 
-solver= Solver.new
+solver = Solver.new
 puts solver.factorial(4)
-puts solver.reverse("Promise")
-puts solver.reverse("Addis")
+puts solver.reverse('Promise')
+puts solver.reverse('Addis')
+puts solver.fizzbuzz(15)
+puts solver.fizzbuzz(30)
+puts solver.fizzbuzz(3)
+puts solver.fizzbuzz(6)
+puts solver.fizzbuzz(5)
