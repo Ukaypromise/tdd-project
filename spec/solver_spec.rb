65c685fc72f5333b2e38cddabc84f5bf0e7aaf5f
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-11-10 20:37:41 +0100

    Write test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,6 +7,13 @@ describe Solver do
         it 'returns the factorial of all integer from 1 to N' do
         expect(solver.factorial(3)).to eql(6)
         expect(solver.factorial(0)).to eql(1)
+        expect(solver.factorial(-1)).to eql('No Negative Integers')
         end
     end
+
+    describe '#reverse' do
+      it 'it should return the reverse of a string'
+      expect(solver.reverse('Promise')).to be('esimorP')
+    end
+
 end
